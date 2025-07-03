763bdb9b1df54b2967e119f90cce9ea63fcfbefb
TheBaytur <bayturcodding@gmail.com>
2025-07-03 17:24:12 +0800

    Offices

---
+++ lib/offices.dart
@@ -0,0 +1,33 @@
+class OfficesList {
+  List<Office> offices;
+  OfficesList({required this.offices});
+}
+
+class Office {
+  String name;
+  String address;
+  String city;
+  String country;
+  String lat;
+  String lng;
+
+  Office({
+    required this.name,
+    required this.address,
+    required this.city,
+    required this.country,
+    required this.lat,
+    required this.lng,
+  });
+
+  factory Office.fromJson(Map<String, dynamic> json) {
+    return Office(
+      name: json['name'] as String,
+      address: json['address'] as String,
+      city: json['city'] as String,
+      country: json['country'] as String,
+      lat: json['lat'] as String,
+      lng: json['lng'] as String,
+    );
+  }
+}
