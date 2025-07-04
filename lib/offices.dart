fd0c0fa5abdc1ec4da41ed1610a4d856f97da538
TheBaytur <bayturcodding@gmail.com>
2025-07-04 16:10:39 +0800

    OfficesList

--- lib/offices.dart
+++ lib/offices.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+
 class OfficesList {
   List<Office> offices;
   OfficesList({required this.offices});
@@ -39,5 +44,17 @@ class Office {
       lng: json['lng'] as String,
       image: json['image'] as String,
     );
+  }
+}
+
+Future<OfficesList> getOfficesList() async {
+  final url = Uri.parse('https://about.google/static/data/locations.json');
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    
+    return OfficesList.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load offices: ${response.statusCode}');
   }
 }
